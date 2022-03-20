22814d1929270ca83d8a48f05e681e5874f6d79a
MartinLykke <martin@wirksomhed.dk>
2022-03-20 12:37:56 +0100

    small changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import LandingPage from './LandingPage';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg='success' variant='dark'>
+        <Container>
+          <Navbar.Brand href='#home'>Smart garden frontend</Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
